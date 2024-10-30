c414878d548e8e8edc4f2494b6ca27f4a800ef6e
Christian Servin Ruiz <140107252+ChristianServinRGithub@users.noreply.github.com>
2024-10-29 18:50:42 -0600

    Se trabajo en el layout para dar estructura, creando los componentes principales y se añadio carpeta con imagenes svg

---
+++ src/componets/compartidos/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer>
+       <small>Desarrollado y diseñado por Christian S 2024. Todos los derechos reservados</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
