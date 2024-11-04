1234bb8bde143e5169ff1838010adf615b3302d8
Christian Servin Ruiz <140107252+ChristianServinRGithub@users.noreply.github.com>
2024-11-04 11:56:26 -0600

    Se instalo tailwindcss-neomorphism y se dio la estructura al componente Meta, ademas se instalo simple snippets en vscode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Encabezamiento from "./componets/compartidos/Encabezado";
 import Pie from "./componets/compartidos/Pie";
 import Principal from "./componets/compartidos/Principal";
+import Meta from "./componets/lista/Meta";
 
 function App() {
   return (
     <div className="App">
-     <Encabezamiento></Encabezamiento>
-     <Principal></Principal>
-     <Pie></Pie>
+      <Encabezamiento></Encabezamiento>
+      <Principal>
+        <Meta></Meta>
+      </Principal>
+      <Pie></Pie>
     </div>
   );
 }
